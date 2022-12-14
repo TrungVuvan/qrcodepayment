9ca67e29325ce5267458754cd590e105f0b2f458
HyperIoTTeam <trungvu301020@gmail.com>
2022-12-14 15:28:25 +0700

    qrcode2

---
+++ app.js
@@ -0,0 +1,59 @@
+let max, at_OK;
+let bienx = [];
+let bieny_sin = [];
+let bieny_cos = [];
+
+function makeid() {
+    let text = "";
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < 5; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+let client = new Paho.MQTT.Client("broker.hivemq.com", 8000, makeid());
+
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+
+let options = {
+    useSSL: false,
+    userName: "",
+    password: "",
+    onSuccess: onConnect,
+    onFailure: doFail
+}
+
+console.log("Connect to broker.hivemq.com:8000");
+
+client.connect(options);
+function doFail(e) {
+    console.log(e);
+}
+
+function onConnect() {
+    client.subscribe("Graph");
+    console.log("Connected to topic Graph");
+    client.subscribe("ControlLED");
+    console.log("Connected to topic ControlLED");
+}
+
+function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+        console.log(responseObject.errorMessage);
+    }
+}
+
+function onMessageArrived(message) {
+    if (message.destinationName == 'Graph') {
+        
+    }
+}
+
+function public(topic, data) {
+    message = new Paho.MQTT.Message(data);
+    message.destinationName = topic;
+    console.log(topic + ":" + data);
+    client.send(message);
+}
